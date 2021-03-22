617475b42e3fde3d37d1b1fb7d3ff71fca15e9d3
Rakshitshetty123 <rakshit.shetty@lntinfotech.com>
2021-03-22 15:16:18 +0530

    error code

--- jenkin/Program.cs
+++ jenkin/Program.cs
@@ -10,7 +10,7 @@ namespace jenkin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World")
         }
     }
 }
